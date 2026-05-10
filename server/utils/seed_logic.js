70c66f507501aac7d811e1113c5977ff543b881f
Evoimene <evoimeneelvo@gmail.com>
2026-05-10 19:17:27 +0200

    Ispravljen seeding: dodata organizacija za admina

--- server/utils/seed_logic.js
+++ server/utils/seed_logic.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const Organization = require('../models/Organization');
 const Setting = require('../models/Setting');
 const ShiftType = require('../models/ShiftType');
 
@@ -6,22 +7,39 @@ const seedDatabase = async () => {
   try {
     console.log('Provjera baze za seeding...');
 
-    // 1. Kreiranje admin korisnika
-    const adminExists = await User.findOne({ username: 'admin' });
-    if (!adminExists) {
-      const admin = new User({
+    // 1. Kreiranje podrazumijevane organizacije
+    let organization = await Organization.findOne({ slug: 'glavna-organizacija' });
+    if (!organization) {
+      organization = new Organization({
+        name: 'Glavna Organizacija',
+        slug: 'glavna-organizacija'
+      });
+      await organization.save();
+      console.log('Organizacija kreirana.');
+    }
+
+    // 2. Kreiranje admin korisnika
+    let admin = await User.findOne({ username: 'admin' });
+    if (!admin) {
+      admin = new User({
         username: 'admin',
         password: 'admin123password',
-        role: 'admin'
+        role: 'admin',
+        organizationId: organization._id
       });
       await admin.save();
       console.log('Admin korisnik kreiran: admin / admin123password');
+    } else if (!admin.organizationId) {
+      admin.organizationId = organization._id;
+      await admin.save();
+      console.log('Admin korisnik povezan sa organizacijom.');
     }
 
-    // 2. Osnovne postavke
-    const settingsExists = await Setting.findOne();
+    // 3. Osnovne postavke
+    const settingsExists = await Setting.findOne({ organizationId: organization._id });
     if (!settingsExists) {
       await Setting.create({
+        organizationId: organization._id,
         minRestHours: 11,
         maxHoursPerWeek: 40,
         workDays: 7
@@ -29,19 +47,19 @@ const seedDatabase = async () => {
       console.log('Osnovne postavke kreirane.');
     }
 
-    // 3. Osnovne smjene
-    const shiftsCount = await ShiftType.countDocuments();
+    // 4. Osnovne smjene
+    const shiftsCount = await ShiftType.countDocuments({ organizationId: organization._id });
     if (shiftsCount === 0) {
       const defaultShifts = [
-        { name: 'Prva smjena', start: '07:00', end: '15:00', color: '#3b82f6', weight: 1 },
-        { name: 'Druga smjena', start: '15:00', end: '23:00', color: '#10b981', weight: 1 },
-        { name: 'Noćna smjena', start: '23:00', end: '07:00', color: '#6366f1', weight: 1.5 }
+        { name: 'Prva smjena', start: '07:00', end: '15:00', color: '#3b82f6', organizationId: organization._id },
+        { name: 'Druga smjena', start: '15:00', end: '23:00', color: '#10b981', organizationId: organization._id },
+        { name: 'Noćna smjena', start: '23:00', end: '07:00', color: '#6366f1', organizationId: organization._id }
       ];
       await ShiftType.insertMany(defaultShifts);
       console.log('Osnovne smjene kreirane.');
     }
 
-    console.log('Seeding provjera završena.');
+    console.log('Seeding provjera završena uspješno.');
   } catch (err) {
     console.error('Greška pri seedingu:', err.message);
   }
