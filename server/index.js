a1b7fc08c5f521c2a405f113443d24f39fa1de41
Evoimene <evoimeneelvo@gmail.com>
2026-06-08 20:59:56 +0200

    Fix 413 Payload Too Large error by increasing JSON limit

--- server/index.js
+++ server/index.js
@@ -22,7 +22,8 @@ connectDB().then(() => {
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(morgan('dev'));
 
 // Routes
